5844116dda760e2828a9391e29a4c69f41a42b01
Laura Foscarini <laura-fetter@educar.rs.gov.br>
2024-11-23 17:48:47 -0300

    atualização para o vercel

--- backend/models/Armario.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const armarioSchema = new mongoose.Schema({
-  numero: String,
-  localizacao: String,
-});
-
-module.exports = mongoose.model('Armario', armarioSchema);
